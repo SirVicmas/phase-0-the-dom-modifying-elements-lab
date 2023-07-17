74882f9116fc4b10c041bc6f9d3e158e1dc5904b
Masibo <victor.wafula@student.moringaschool.com>
2023-07-17 11:19:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.querySelector('h1#victory');
+
+const yourName = "Masibo"; 
+    newHeader.innerHTML = `${yourName} is the champion`;
